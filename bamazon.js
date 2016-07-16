51c4be602c96a05291fc616665cb0f0afb285ec9
Rhea Rivera <rjuner@gmail.com>
2016-07-16 17:58:15 -0500

    tables work now

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql'); 
 var inquirer = require('inquirer'); 
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
 	host: 'localhost', 
@@ -14,19 +15,43 @@ connection.connect(function(err){
 }); 
 
 function selection(){
-	inquirer.prompt([
-		{
-			type: "input", 
-			message: "Enter ID of item you want to purchase: ", 
-			name: "userSelection"
-		}, 
-		{
-			type: "input", 
-			message: "Enter quantity of items you want to purchase: ",
-			name: "userQuantity"
+	connection.query('SELECT * FROM Products', function(err, res) {
+	    if (err) throw err;
+
+	    var table = new Table({
+	    	head: ["Item ID", "Product Name", "Department Name", "Price", "Qty"], 
+	    	colWidths: [10, 40, 20, 13, 13],
+	    });
+
+	    for(var i = 0; i < res.length; i++) {
+			table.push(
+			    [res[i].itemId, res[i].productName, res[i].departmentName, parseFloat(res[i].price).toFixed(2), res[i].stockQty]
+			);
 		}
-	]).then(function(user)){
-		switch(user
-	}
+
+		console.log(table.toString());
+
+		inquirer.prompt([
+			{
+				type: "input", 
+				message: "Enter ID of item you want to purchase: ", 
+				name: "userSelection"
+			}, 
+			{
+				type: "input", 
+				message: "Enter quantity of items you want to purchase: ",
+				name: "userQuantity"
+			}
+		]).then(function(user){
+			connection.query('SELECT * FROM Products', function(err, res) {
+				if (err) throw err;
+				console.log(user);
+
+			});
+		});
+
+	});
+
 }
 
+selection();
